e68c40e15331321bd66de2d12017bdc06bb401b8
zietzm <michael.zietz@gmail.com>
2024-07-11 08:18:31 -0500

    feat: add python test for dtype
    Previously, there was an issue in case we passed dtypes that weren't the
    same as the Python FFI function we define in Rust. MDAV is generic, so
    we could use f32 or f64. In this package, though, we use f64 in our Rust
    wrapper. This leads to only numpy arrays with float64 dtype working.
    However, we also want float32, so we wrapped it in a python function
    test that it works either way.

---
+++ python/webgwas/test_mdav.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pytest
+
+from webgwas.mdav import mdav
+
+
+@pytest.mark.parametrize(
+    "dtype",
+    [
+        np.float32,
+        np.float64,
+    ],
+)
+def test_mdav_dtype(dtype):
+    # Check that mdav works regardless of the dtype of the input (float32 or float64)
+    records = np.array(
+        [
+            [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+            [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+            [10, 9, 8, 7, 6, 5, 4, 3, 2, 1],
+        ],
+        dtype=dtype,
+    )
+    # For this test, k=3 with N=3 means they should all be the same (the mean)
+    expected = np.vstack(records.mean(axis=0)).reshape(1, -1)
+    assert np.allclose(mdav(records, 3), expected)
