41503b32ff0f7604fd1dffeadf21005852677221
zietzm <michael.zietz@gmail.com>
2024-08-09 08:56:50 -0500

    feat: add residualize function

--- python/webgwas/regression.py
+++ python/webgwas/regression.py
@@ -1,13 +1,13 @@
 import numpy as np
+import pandas as pd
 from numpy.typing import NDArray
-from pandas import DataFrame, Series
 
 
 def regress_array(y: NDArray, x: NDArray) -> NDArray:
     """Regress y on x.
 
     Args:
-        y: The dependent variable
+        y: The dependent variable(s)
         x: The independent variable(s)
 
     Returns:
@@ -17,7 +17,7 @@ def regress_array(y: NDArray, x: NDArray) -> NDArray:
     return beta
 
 
-def regress(y: Series, x: DataFrame) -> Series:
+def regress(y: pd.Series, x: pd.DataFrame) -> pd.Series:
     """Regress y on x.
 
     Args:
@@ -27,10 +27,10 @@ def regress(y: Series, x: DataFrame) -> Series:
     Returns:
         The regression coefficients
     """
-    return Series(regress_array(np.asarray(y.values), x.values), index=x.columns)
+    return pd.Series(regress_array(np.asarray(y.values), x.values), index=x.columns)
 
 
-def regress_left_inverse(y: Series, left_inverse: DataFrame) -> Series:
+def regress_left_inverse(y: pd.Series, left_inverse: pd.DataFrame) -> pd.Series:
     """Regress y on x using the left inverse of x.
 
     Args:
@@ -40,4 +40,19 @@ def regress_left_inverse(y: Series, left_inverse: DataFrame) -> Series:
     Returns:
         The regression coefficients
     """
-    return Series(left_inverse @ y, index=left_inverse.index)
+    return pd.Series(left_inverse @ y, index=left_inverse.index)
+
+
+def residualize(Y: pd.DataFrame, X: pd.DataFrame) -> pd.DataFrame:
+    """Residualize Y on X.
+
+    Args:
+        Y: The dependent variable(s) (N x p)
+        X: The independent variable(s) (N x m)
+
+    Returns:
+        The residualized Y (N x p)
+    """
+    beta_array = regress_array(Y.values, X.values)
+    beta_df = pd.DataFrame(beta_array, index=X.columns, columns=Y.columns)
+    return Y - X @ beta_df
