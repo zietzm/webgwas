6d8ff116595fec8671ea3ee2502ba844da1b4fb4
zietzm <michael.zietz@gmail.com>
2024-09-30 12:29:27 -0700

    feat: add helper for polars::Series to faer::Col

--- backend/src/utils.rs
+++ backend/src/utils.rs
@@ -1,3 +1,7 @@
+use anyhow::Result;
+use faer::Col;
+use polars::series::Series;
+
 /// Get everything up to and including the item
 pub fn slice_before<T: PartialEq + Clone>(vec: &[T], item: &T) -> Vec<T> {
     if let Some(index) = vec.iter().position(|x| x == item) {
@@ -23,4 +27,13 @@ pub fn slice_before_excl<T: PartialEq + Clone>(vec: &[T], item: &T) -> Vec<T> {
     } else {
         vec.to_vec()
     }
+}
+
+/// Convert a polars Series to a faer Col
+pub fn series_to_col_vector(series: Series) -> Result<Col<f32>> {
+    let mut result = Col::zeros(series.len());
+    series.f32()?.iter().enumerate().for_each(|(i, x)| {
+        result[i] = x.expect("Failed to get value");
+    });
+    Ok(result)
 }
